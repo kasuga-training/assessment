98f201e228bcb77d3805d54011cc2d20f3709446
kasuga-training <kasuga.daiki.tmx@gmail.com>
2025-06-30 17:26:24 +0900

    あなたのいいところ診断ファイルの変更

--- assessment.js
+++ assessment.js
@@ -16,14 +16,27 @@ assessmentButton.addEventListener(
 
 
     resultDivision.innerText = '';// 初期化（id=result-areaの内容）
-    const header = document.createElement('h3');// h3タグを作成
-    header.innerText = '診断結果'; //h3タグの中身に書き込む
-    resultDivision.appendChild(header); //id=result-areaのdivタグの子要素として追加
+    // headerDivision の作成
+    const headerDivision = document.createElement('div');
+    headerDivision.setAttribute('class', 'card-header text-bg-primary');
+    headerDivision.innerText = '診断結果';
 
-    const paragraph = document.createElement('p');// pタグを作成
-    const result = assessment(userName);// 機能を実行
-    paragraph.innerText = result;//結果をpタグの中身に書き込む
-    resultDivision.appendChild(paragraph);//id=result-areaのdivタグに子要素として追加（headerの次に表示させる）
+    // bodyDivision の作成
+    const bodyDivision = document.createElement('div');
+    bodyDivision.setAttribute('class', 'card-body');
+
+    const paragraph = document.createElement('p');
+    paragraph.setAttribute('class', 'card-text');
+    const result = assessment(userName);
+    paragraph.innerText = result;
+    bodyDivision.appendChild(paragraph);
+
+    // resultDivision に Bootstrap のスタイルを適用する
+    resultDivision.setAttribute('class', 'card');
+
+    // headerDivision と bodyDivision を resultDivision に差し込む
+    resultDivision.appendChild(headerDivision);
+    resultDivision.appendChild(bodyDivision);
 
     // ツイートエリアの作成
     // while(tweetDivision.firstChild){ //練習問題
@@ -51,7 +64,7 @@ assessmentButton.addEventListener(
 
 userNameInput.addEventListener('keydown',
   event => {
-    if(event.code === 'Enter'){
+    if (event.code === 'Enter') {
       assessmentButton.dispatchEvent(new Event('click'));
     }
   }
